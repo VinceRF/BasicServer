ec67b588ef583a33b101c4a8862c3bbbf8d01dfa
vincent <zhsk30@onycom.com>
2023-03-27 16:48:09 +0900

    프로젝트 생성

---
+++ src/ServerInitializer.java
@@ -0,0 +1,40 @@
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.*;
+import java.util.List;
+
+public class ServerInitializer {
+
+    public static void main(String[] args) {
+        int port = 50000;
+        System.out.println("SERVER ON :: " + port);
+
+        Reactor reactor = new Reactor(port);
+
+        try {
+            Serializer serializer = new Persister();
+            File source = new File("HandlerList.xml");
+            ServerListData serverListData = serializer.read(ServerListData.class, source);
+
+            for (HandlerListData handlerListData : serverListData.getServer()) {
+                if ("server1".equals(handlerListData.getName())) {
+                    List<HandleData> handleData = handlerListData.getHandler();
+                    for(HandleData handler : handleData) {
+                        try {
+                            reactor.registerHandler(handler.getHeader(), (EventHandler) Class.forName(handler.getHandler()).newInstance());
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        reactor.startServer();
+    }
+}
